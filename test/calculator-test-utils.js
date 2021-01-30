53ca336234e65a4f759a51035e18b0f4f546696c
naoya7076 <naoya7076@gmail.com>
2021-01-30 21:57:52 +0900

    add calculator-test-utils render

---
+++ test/calculator-test-utils.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {render as rtlRender} from '@testing-library/react'
+import {ThemeProvider} from '@emotion/react'
+import {dark} from '../src/themes'
+
+function render(ui, options) {
+  return rtlRender(ui, {wrapper: Wrapper, ...options})
+}
+
+function Wrapper({children}) {
+  return <ThemeProvider theme={dark}>{children}</ThemeProvider>
+}
+Wrapper.propTypes = {
+  children: PropTypes.node,
+}
+
+export * from '@testing-library/react'
+export {render}
