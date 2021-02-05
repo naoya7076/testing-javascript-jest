3b3cf8206808508ad5c25338ed041fa3f33591e6
naoya7076 <naoya7076@gmail.com>
2021-02-05 23:35:01 +0900

    不要なファイルの削除

--- src/shared/auto-scaling-text.js
+++ src/shared/auto-scaling-text.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './auto-scaling-text.module.css'
 
-// commits!
-
 function getScale(node) {
   if (!node) {
     return 1
